c8462671b1a56f22f5b44a8935bbd83df6b26dd5
Abdulaziz <mohammed.abdulazizt.19@gmail.com>
2023-01-24 08:38:48 +0800

    updated the website

---
+++ assets/js/partners.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){
+    $('.partners-logos').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
